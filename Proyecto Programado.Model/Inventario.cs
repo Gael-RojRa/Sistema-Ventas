2fdbbac69cc6c467287a50d4ffbd9981bc597684
Heinner.Villagra <Heinner.Villagra@ucr.ac.cr>
2024-06-09 00:21:15 -0600

    Se creo el historico del inventario

--- Proyecto Programado.Model/Inventario.cs
+++ Proyecto Programado.Model/Inventario.cs
@@ -13,11 +13,12 @@ namespace Proyecto_Programado.Model
     {
 
         public int Id { get; set; }
-        //[Required(ErrorMessage = "El nombre es requerido")]
+        [Required(ErrorMessage = "El nombre es requerido")]
         public string Nombre { get; set; }
         [Required(ErrorMessage = "La categoría es requerida")]
         public Categoria Categoria { get; set; }
         public int Cantidad { get; set; } = 0;
+
         [Required(ErrorMessage = "El precio es requerido")]
         [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0")]
         public decimal Precio { get; set; }

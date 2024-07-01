190887d99893d072efff9337dd27708337595a3a
GERARDO GAEL ROJAS RAMOS <gerardo.rojasramos@ucr.ac.cr>
2024-07-01 16:10:39 -0600

    Modificación del proyecto SI

---
+++ ProyectoProgramado.SI/Controllers/ModuloCatalogoDeInventariosController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Programado.BL;
+
+namespace Proyecto_Programado.SI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ModuloCatalogoDeInventariosController : ControllerBase
+    {
+        private readonly Proyecto_Programado.BL.IAdministradorDeInventarios ElAdministradorDeInventarios;
+
+        public ModuloCatalogoDeInventariosController(IAdministradorDeInventarios elAdministradorDeInventarios)
+        {
+
+            ElAdministradorDeInventarios = elAdministradorDeInventarios;
+
+        }
+
+        [HttpGet("ObtengaLaListaDeInventarios")]
+        public IActionResult ObtengaLaListaDeInventarios()
+        {
+            return Ok(ElAdministradorDeInventarios.ObtengaLaListaDeInventarios());
+        }
+
+        [HttpPost("AgregueElInventario")]
+        public IActionResult AgregueElInventario([FromBody] Proyecto_Programado.Model.Inventario elInventario, string elNombreDeUsuario)
+        {
+            ElAdministradorDeInventarios.AgregueElInventario(elInventario, elNombreDeUsuario);
+            return Ok();
+        }
+
+        [HttpGet("ObtengaElInventario")]
+        public IActionResult ObtengaElInventario(int elId)
+        {
+            return Ok(ElAdministradorDeInventarios.ObtengaElInventario(elId));
+        }
+
+        [HttpPost("EditeElInventario")]
+        public IActionResult EditeElInventario([FromBody] Proyecto_Programado.Model.Inventario elInventario, string elNombreDeUsuario)
+        {
+            ElAdministradorDeInventarios.EditeElInventario(elInventario, elNombreDeUsuario);
+            return Ok();
+        }
+
+        [HttpGet("ObtengaInventarioConHistorico")]
+        public IActionResult ObtengaInventarioConHistorico(int elId)
+        {
+            return Ok(ElAdministradorDeInventarios.ObtengaInventarioConHistorico(elId));
+        }
+    }
+}
---
+++ ProyectoProgramado.SI/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<Proyecto_Programado.BL.IAdministradorDeUsuarios, Proyecto_Programado.BL.AdministradorDeUsuarios>();
+builder.Services.AddScoped<Proyecto_Programado.BL.IAdministradorDeInventarios, Proyecto_Programado.BL.AdministradorDeInventarios>();
+builder.Services.AddScoped<Proyecto_Programado.BL.IAdministradorDeAjustes, Proyecto_Programado.BL.AdministradorDeAjustes>();
+builder.Services.AddScoped<Proyecto_Programado.BL.IAdministradorDeCaja, Proyecto_Programado.BL.AdministradorDeCaja>();
+builder.Services.AddScoped<Proyecto_Programado.BL.IAdministradorDeVentas, Proyecto_Programado.BL.AdministradorDeVenta>();
+builder.Services.AddScoped<Proyecto_Programado.BL.IAdministradorDeSolicitudes, Proyecto_Programado.BL.AdministradorDeSolicitudes>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Proyecto_Programado.DA.DBContexto>(x => x.UseSqlServer(connectionString));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+})
+.AddCookie(options =>
+{
+    options.LoginPath = "/Login/InicieSesion";
+    options.AccessDeniedPath = "/Login/InicieSesion";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

6f4f22078c36b472a74386755c94b17ec5b2d0d3
Heinner.Villagra <Heinner.Villagra@ucr.ac.cr>
2024-07-05 00:06:44 -0600

    Se modifica el login para facebook para añadirle la funcionalidad de suscripcion

--- Proyecto Programado.UI/Controllers/SolicitudDeRegistrosController.cs
+++ Proyecto Programado.UI/Controllers/SolicitudDeRegistrosController.cs
@@ -8,6 +8,7 @@ using Proyecto_Programado.UI.ViewModels;
 using System.Security.Claims;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Facebook;
 
 namespace Proyecto_Programado.UI.Controllers
 {
@@ -77,6 +78,64 @@ namespace Proyecto_Programado.UI.Controllers
            
 
         }
+
+        public async Task<IActionResult> LoginWithFacebook()
+        {
+            try
+            {
+                var redirectUrl = Url.Action("FacebookResponse", "SolicitudDeRegistros");
+                var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+                return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+            }
+            catch (Exception ex)
+            {
+
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        public async Task<IActionResult> FacebookResponse()
+        {
+            var resultado = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            if (!resultado.Succeeded)
+            {
+                return Unauthorized();
+            }
+
+            var claimsPrincipal = resultado.Principal;
+
+            var nameClaim = claimsPrincipal.FindFirst(ClaimTypes.Name);
+            var emailClaim = claimsPrincipal.FindFirst(ClaimTypes.Email)?.Value;
+
+            if (nameClaim == null)
+            {
+                return BadRequest("Claim de Name no encontrado");
+            }
+
+            var usuarioName = nameClaim.Value;
+            var usuarioEmail = emailClaim;
+
+            var usuarioExistente = ElAdministrador.ObtengaElUsuarioPorNombre(usuarioName);
+
+            if (usuarioExistente == null)
+            {
+                ElAdministrador.CrearUsuario(usuarioName, usuarioEmail);
+                return RedirectToAction("SolicitudPendiente", "SolicitudDeRegistros");
+            }
+            else
+            {
+                if (usuarioExistente.EstadoRegistro == EstadoRegistro.Pendiente)
+                {
+                    return RedirectToAction("SolicitudPendiente", "SolicitudDeRegistros");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Inventario");
+                }
+            }
+        }
+
 
         public IActionResult SolicitudPendiente()
         {
